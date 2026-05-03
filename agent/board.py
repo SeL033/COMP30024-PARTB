87e06bebb25e70d09c340b8508f4670af4005231
Uniminh <huuminht@student.unimelb.edu.au>
2026-05-03 13:42:01 +1000

    remove magic numbers

--- agent/board.py
+++ agent/board.py
@@ -2,10 +2,15 @@ from referee.game.constants import BOARD_N, MAX_TURNS, PLACEMENT_TURNS, INITIAL_
 from referee.game import PlaceAction, MoveAction, EatAction, CascadeAction, Coord, Direction, PlayerColor
 
 CENTRE = 128
+BOARD_N_2 = BOARD_N * BOARD_N
+
 PLACE = "P"
 MOVE = "M"
 EAT = "E"
 CASCADE = "C"
+
+DIRECTIONS = ((-BOARD_N, Direction.Up, 0), (BOARD_N, Direction.Down, 0), (-1, Direction.Left, -1), (1, Direction.Right, 1))
+
 
 class State:
     __slots__ = ("board",
@@ -19,7 +24,7 @@ class State:
              "prev_actions",)
     
     def __init__(self):
-        self.board = bytearray([CENTRE] * 64)
+        self.board = bytearray([CENTRE] * BOARD_N_2)
         self.red_tokens = 0
         self.red_stacks = 0
         self.blue_tokens = 0
@@ -30,17 +35,17 @@ class State:
         self.prev_actions = []
 
     def _apply_place(self, coord):
-        board_index = coord.r*8 + coord.c
+        board_index = coord.r*BOARD_N + coord.c
         was_red = self.red_turn
         action_log = (PLACE, board_index, was_red)
 
         if was_red :
-            self.board[board_index] += 3
-            self.red_tokens += 3
+            self.board[board_index] += INITIAL_STACK_HEIGHT
+            self.red_tokens += INITIAL_STACK_HEIGHT
             self.red_stacks += 1
         else :
-            self.board[board_index] -= 3
-            self.blue_tokens += 3
+            self.board[board_index] -= INITIAL_STACK_HEIGHT
+            self.blue_tokens += INITIAL_STACK_HEIGHT
             self.blue_stacks += 1
 
         self.red_turn = not was_red
@@ -48,10 +53,10 @@ class State:
         self.prev_actions.append(action_log)
 
     def _apply_move(self, coord, direction):
-        board_index = coord.r*8 + coord.c
+        board_index = coord.r*BOARD_N + coord.c
         was_red = self.red_turn
         move_stack_delta = self.board[board_index] - CENTRE
-        target_index = board_index + direction.r*8 + direction.c
+        target_index = board_index + direction.r*BOARD_N + direction.c
         action_log = (MOVE, board_index, was_red, move_stack_delta, target_index)
 
         self.board[board_index] = CENTRE
@@ -67,9 +72,9 @@ class State:
         self.prev_actions.append(action_log)
 
     def _apply_eat(self, coord, direction):
-        board_index = coord.r*8 + coord.c
+        board_index = coord.r*BOARD_N + coord.c
         was_red = self.red_turn
-        target_index = board_index + direction.r*8 + direction.c
+        target_index = board_index + direction.r*BOARD_N + direction.c
         eaten_stack_delta = self.board[target_index] - CENTRE
         action_log = (EAT, board_index, was_red, eaten_stack_delta, target_index)
         
@@ -87,13 +92,13 @@ class State:
         self.prev_actions.append(action_log)
     
     def _apply_cascade(self, coord, direction):
-        board_index = coord.r*8 + coord.c
+        board_index = coord.r*BOARD_N + coord.c
         was_red = self.red_turn
         tokens_and_stacks = (self.red_tokens, self.red_stacks, self.blue_tokens, self.blue_stacks)
 
         save_scan = [(board_index, self.board[board_index])]
-        row = board_index // 8
-        increment = direction.r*8 + direction.c
+        row = board_index // BOARD_N
+        increment = direction.r*BOARD_N + direction.c
         cascade_len = abs(self.board[board_index] - CENTRE)
         single_stack = (CENTRE + 1) if was_red else (CENTRE - 1)
         next_index = board_index + increment
@@ -107,15 +112,15 @@ class State:
         # board_index gets mutated directly
         while True:
             board_index += increment
-            if not (0 <= board_index < 64):
+            if not (0 <= board_index < BOARD_N_2):
                 break
-            if (direction.c != 0) and ((board_index // 8) != row):
+            if (direction.c != 0) and ((board_index // BOARD_N) != row):
                 break
             save_scan.append((board_index, self.board[board_index]))
         action_log = (CASCADE, was_red, tuple(save_scan), tokens_and_stacks)
 
         for i in range(cascade_len):
-            if (not (0 <= next_index < 64)) or ((direction.c != 0) and (next_index // 8 != row)):
+            if (not (0 <= next_index < BOARD_N_2)) or ((direction.c != 0) and (next_index // BOARD_N != row)):
                 token_lost = cascade_len - i
                 if was_red:
                     self.red_tokens -= token_lost
@@ -142,7 +147,7 @@ class State:
         current_cell = self.board[board_index]
         next_index = board_index + increment
 
-        if (not (0 <= next_index < 64)) or ((dir_c != 0) and (next_index // 8 != row)):
+        if (not (0 <= next_index < BOARD_N_2)) or ((dir_c != 0) and (next_index // BOARD_N != row)):
             if current_cell > CENTRE:
                 self.red_tokens -= current_cell - CENTRE
                 self.red_stacks -= 1
@@ -179,10 +184,10 @@ class State:
             self.placement_count -= 1
             self.red_turn = was_red
             if was_red :
-                self.red_tokens -= 3
+                self.red_tokens -= INITIAL_STACK_HEIGHT
                 self.red_stacks -= 1
             else :
-                self.blue_tokens -= 3
+                self.blue_tokens -= INITIAL_STACK_HEIGHT
                 self.blue_stacks -= 1
             self.board[board_index] = CENTRE
 
@@ -225,41 +230,40 @@ class State:
     def legal_actions(self):
         legal_list = []
         if self.placement_count == 0:
-            for i in range(64):
-                legal_list.append(PlaceAction(Coord(i // 8, i % 8)))
+            for i in range(BOARD_N_2):
+                legal_list.append(PlaceAction(Coord(i // BOARD_N, i % BOARD_N)))
         elif self.placement_count < PLACEMENT_TURNS:
             is_red = self.red_turn
-            for i in range(64):
+            for i in range(BOARD_N_2):
                 if self.board[i] != CENTRE: continue
-                r, c = i // 8, i % 8
+                r, c = i // BOARD_N, i % BOARD_N
                 if is_red:
-                    if (r > 0) and (self.board[i - 8] < CENTRE): continue
-                    if (r < 7) and (self.board[i + 8] < CENTRE): continue
+                    if (r > 0) and (self.board[i - BOARD_N] < CENTRE): continue
+                    if (r < BOARD_N - 1) and (self.board[i + BOARD_N] < CENTRE): continue
                     if (c > 0) and (self.board[i - 1] < CENTRE): continue
-                    if (c < 7) and (self.board[i + 1] < CENTRE): continue
+                    if (c < BOARD_N - 1) and (self.board[i + 1] < CENTRE): continue
                 else:
-                    if (r > 0) and (self.board[i - 8] > CENTRE): continue
-                    if (r < 7) and (self.board[i + 8] > CENTRE): continue
+                    if (r > 0) and (self.board[i - BOARD_N] > CENTRE): continue
+                    if (r < BOARD_N - 1) and (self.board[i + BOARD_N] > CENTRE): continue
                     if (c > 0) and (self.board[i - 1] > CENTRE): continue
-                    if (c < 7) and (self.board[i + 1] > CENTRE): continue
+                    if (c < BOARD_N - 1) and (self.board[i + 1] > CENTRE): continue
                 legal_list.append(PlaceAction(Coord(r, c)))
         else:
             is_red = self.red_turn
-            directions = ((-8, Direction.Up, 0), (8, Direction.Down, 0), (-1, Direction.Left, -1), (1, Direction.Right, 1))
             
-            for i in range(64):
+            for i in range(BOARD_N_2):
                 if is_red:
                     if self.board[i] <= CENTRE: continue
                 else:
                     if self.board[i] >= CENTRE: continue
 
-                r, c = i // 8, i % 8
+                r, c = i // BOARD_N, i % BOARD_N
                 current_cell = self.board[i] - CENTRE
 
-                for direction in directions:
+                for direction in DIRECTIONS:
                     target_index = i + direction[0]
-                    if not (0 <= target_index < 64): continue
-                    if (direction[2] != 0) and (target_index // 8 != r): continue
+                    if not (0 <= target_index < BOARD_N_2): continue
+                    if (direction[2] != 0) and (target_index // BOARD_N != r): continue
 
                     target_cell = self.board[target_index] - CENTRE
 
